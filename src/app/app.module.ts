7cd8114769ffb51baa1421f55ddedf117c798b65
[Sergi Bosque Ródenas] <[sbosquero@uoc.edu]>
2024-03-02 13:56:05 +0100

    sign in form logic done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
--- src/app/models/user.dto.ts
+++ src/app/models/user.dto.ts
@@ -1,10 +1,18 @@
 export class UserDTO{
   email: string;
   password: string;
+  name?: string;
+  surname?: string;
+  alias?: string;
+  birth?: Date;
 
-  constructor(email: string, password: string){
+  constructor(email: string, password: string, name?: string, surname?: string, alias?: string, birth?: Date){
     this.email = email;
     this.password = password;
+    this.name = name;
+    this.surname = surname;
+    this.alias = alias;
+    this.birth = birth;
   }
 
 }
