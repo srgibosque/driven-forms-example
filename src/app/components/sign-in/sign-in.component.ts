c85bf2742513ec02129532baab9c3d34cf61d170
[Sergi Bosque Ródenas] <[sbosquero@uoc.edu]>
2024-03-02 15:24:54 +0100

    sign in form template done

--- src/app/components/sign-in/sign-in.component.ts
+++ src/app/components/sign-in/sign-in.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { UserDTO } from '../../models/user.dto';
 
 @Component({
   selector: 'app-sign-in',
@@ -6,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrl: './sign-in.component.css'
 })
 export class SignInComponent {
+  user: UserDTO;
+
+  constructor(){
+    this.user = new UserDTO('', '', '', '', '', new Date());
+  }
+
+  onSubmit(form: NgForm){
+    if(form.valid){
+      console.log('Email:'+ this.user.email);
+      console.log('Password:'+ this.user.password);
+      console.log('Alias:'+ this.user.alias);
+      console.log('Name:'+ this.user.name);
+      console.log('Surname:'+ this.user.surname);
+      console.log('Birth:'+ this.user.birth);
+    } else {
+      console.log('Invalid form');
+    }
+  }
 
 }
